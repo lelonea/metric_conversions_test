7bdcba2ce84bde793b427721b199fe774fd3baf4
Nastya <leloneya@gmail.com>
2021-05-28 16:04:42 +0300

    Made functions universal for all types. Added full check function

--- task3.py
+++ task3.py
@@ -6,19 +6,25 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 wait = WebDriverWait(browser, 20)
+temperature = '//*[@id="typeMenu"]/a[1]'
+cf = '//*[@id="popLinks"]/ol/li[1]/a'
+length = '//*[@id="typeMenu"]/a[3]'
+mf = '//*[@id="popLinks"]/ol/li[1]/a'
+weight = '//*[@id="typeMenu"]/a[2]'
+og = '//*[@id="popLinks"]/ol/li[5]/a'
 
 
 def main_page():
     browser.get('https://www.metric-conversions.org/')
 
 
-def open_conv_cf():
-    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mainLinks"]/a[3]')))
-    calc = browser.find_element_by_xpath('//*[@id="mainLinks"]/a[3]')
+def open_conv(xpath):
+    wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    calc = browser.find_element_by_xpath(xpath)
     calc.click()
 
 
-def check_conv_cf(a):
+def check_conv(a):
     field = browser.find_element_by_xpath('//*[@id="argumentConv"]')
     field.send_keys(a)
 
@@ -34,3 +40,15 @@ def check_is_num():
 
 def quit_browser():
     browser.quit()
+
+
+def check(type_conv, conv_xpath):
+    main_page()
+    open_conv(type_conv)
+    open_conv(conv_xpath)
+    check_conv(check_is_num())
+
+
+check(temperature, cf)
+check(length, mf)
+check(weight, og)
